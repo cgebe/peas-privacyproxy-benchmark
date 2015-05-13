c0fa99c20465c308d8fc1d942a67e8e7855e4b88
unknown <gegnazz@yahoo.de>
2015-05-13 13:14:14 +0200

    onion

--- src/onionrouter/node/HandshakeHandler.java
+++ src/onionrouter/node/HandshakeHandler.java
@@ -1,27 +1,27 @@
 package onionrouter.node;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import protocol.PEASBody;
+import protocol.PEASHeader;
+import protocol.PEASObject;
+import protocol.PEASResponse;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 
-public class HandshakeHandler implements ChannelHandler {
+public class HandshakeHandler extends SimpleChannelInboundHandler<PEASObject> {
 
 	@Override
-	public void exceptionCaught(ChannelHandlerContext arg0, Throwable arg1)
+	protected void channelRead0(ChannelHandlerContext arg0, PEASObject arg1)
 			throws Exception {
 		// TODO Auto-generated method stub
-
+		
 	}
-
-	@Override
-	public void handlerAdded(ChannelHandlerContext arg0) throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void handlerRemoved(ChannelHandlerContext arg0) throws Exception {
-		// TODO Auto-generated method stub
-
-	}
+	
+	
 
 }
